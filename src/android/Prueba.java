5635fbebf4908bfce5b1bd0904e0a73ea4dd926d
Ivan Paredes <ip@airtrack.com.ar>
2017-12-20 11:33:03 +0000

    con comentarios y finalizado

--- src/android/Prueba.java
+++ src/android/Prueba.java
@@ -1,50 +1,57 @@
 
-
-package com.example.plugin;
+//en packpage debe ir la ruta donde se encuentra este archivo, pero separada por puntos
+package com.paredesivan.plugin;
 
 //esos 3 van si o si
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.JSONArray;  //obligatorio
+import org.json.JSONException; //obligatorio
 import org.apache.cordova.*;
-import android.provider.Settings;
 
-
+//la clase publica debe tener el mismo nombre que el archivo
 public class Prueba extends CordovaPlugin {
-
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("obtenerDatosJava")) {
+        //el nombre con el que se llama desde el exec del archivo js
+        if (action.equals("saludo")) {
 
-                    String name = args.getString(0);
-                    String message = "Hello, " + name;
-                    callbackContext.success(message);
 
-                    return true;
+           String nombre = args.getString(0); //obtiene el estring de la primer llave del JSONArray
+           String mensaje = "Hola, " + nombre;
 
-                } else {
+           //la devolucion que le da al js
+           callbackContext.success(mensaje);
 
-                    return false;
+           return true;
 
-                }
+        }
+
+
+        if (action.equals("obtenerNombreDispositivo")){
+            callbackContext.success(obtenerNombreDispositivo());
+            return true;
+        }
+
+        else {
+           return false;
+        }
 
     }
 
 
     //metodo que brinda el nombre
     private String obtenerNombreDispositivo() {
-          //String manufacturer = android.os.Build.MANUFACTURER;
-          //String model = android.os.Build.MODEL;
 
-          //if (model.startsWith(manufacturer)) {
-          //    return model;
-          //} else {
-          //    return manufacturer + " " + model;
-          //}
+        String manufacturer = android.os.Build.MANUFACTURER;
+        String model = android.os.Build.MODEL;
 
-          return "hola";
+        if (model.startsWith(manufacturer)) {
+          return model;
+        } else {
+          return manufacturer + " " + model;
+        }
+
     }
 }
 
--- www/prueba.js
+++ www/prueba.js
@@ -1,33 +1,34 @@
-//var exec = require('cordova/exec');
-//
-//module.exports = {
-//
-//    obtenerDatos: function (name,successCallback, errorCallback) {
-//
-//        //exec(successfunction, failfuncion, service, action, [args])
-//        //service debe ser el nombre del declarado en el xml y el nombre de la clase
-//        //action el nombre que sera tratado en el execute de java
-//        //args son parametros de entrada a la funcion java
-//        exec(successCallback, errorCallback, 'Prueba', 'obtenerDatosJava', [name]);
-//    }
-//
-//    //ejemplo
-//    //    cordova.exec(successCallback, funcion cuando el resultado es exitoso
-//    //        errorCallback,     funcion cuando el resultado no es exitoso
-//    //        "SendMail",        clase
-//    //        "send",            metodo o funcion java
-//    //        [{                 array
-//    //            "subject":subject,
-//    //            "body":body,
-//    //            "sender":sender,
-//    //            "password":password,
-//    //            "recipients":recipients
-//    //        }]
-//    //    );
-//};
+var exec = require('cordova/exec');
 
 module.exports = {
-    obtenerDatos: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Prueba", "obtenerDatosJava", [name]);
+
+    saludar: function (name, exito, falla) {
+
+        //exec(successfunction, failfuncion, service, action, [args])
+        //service debe ser el nombre del declarado en el xml y el nombre de la clase
+        //action el nombre que sera tratado en el execute de java. PUEDE ser igual
+        // al nombre de la funcion de aca
+        //args son parametros de entrada a la funcion java
+        exec(exito, falla, 'pruebita', 'saludo', [name]);
+    },
+
+    obtenerNombreDispositivo: function (successCallback, errorCallback) {
+
+        exec(successCallback, errorCallback, 'pruebita', 'obtenerNombreDispositivo', []);
     }
+
+    //ejemplo
+    //    cordova.exec(successCallback, funcion cuando el resultado es exitoso
+    //        errorCallback,     funcion cuando el resultado no es exitoso
+    //        "SendMail",        clase
+    //        "send",            metodo o funcion java
+    //        [{                 array
+    //            "subject":subject,
+    //            "body":body,
+    //            "sender":sender,
+    //            "password":password,
+    //            "recipients":recipients
+    //        }]
+    //    );
 };
+
